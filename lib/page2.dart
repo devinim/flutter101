fa05b148c215fa70b81db6b923d79b322c568092
Furkan S. Bulut <furkanbulut@gmail.com>
2020-02-26 10:14:14 +0300

    Page 2

---
+++ lib/page2.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+
+    return null;
+  }
+}
+
+
+class _Page2State extends State<Page2> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Merhaba"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Text("Child item 1"),
+          Text("Child item 2"),
+          Text("Child item 3")
+        ],
+      ),
+    );
+
+  }
+
+
+}
